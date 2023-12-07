2b59ca834e21802d145e576f85607f2c789b3701
ikrama10 <ikramakhan123@gmail.com>
2023-12-07 19:14:48 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-<h1>
-  hello
-  
-</h1>
+      <h1>hello</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
